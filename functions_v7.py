7c91176e4d8fa03964ae752bf02b8893386e4569
Blumenfreund1337 <maxiwojak2407@gmail.com>
2024-07-16 18:25:03 +0200

    final commit

--- functions_v7.py
+++ functions_v7.py
@@ -193,7 +193,7 @@ def pidsofSessions(roi = ""):
 test1 = createSess(pid="5e8ac11b-959a-49ab-a6a3-8a3397e1df0e", POn=True)
 Acros = test1.getAcronymInfo()
 print(Acros)  
-
+------------------------------
 #case2
 roi = "SNr" # Region Of Interest (acronym according to Allen Atlas)
 test2= createSess(pid = '6a7544a8-d3d4-44a1-a9c6-7f4d460feaac', POn=True)
@@ -201,7 +201,7 @@ print(test2.getMainInfo())
 test2.getLineGraph(events1='stimOn_times', Roi = roi, xlab="Time from Stimulus Onset (s)", ylab="spikes/s", intervall1=[0.5,2], error_bars1="sem",
                  pidnmb=2)
 plt.show() 
-
+------------------------------
 #case3 
 roi2 = "CP"
 test3 = createSess(Roi= roi2, POn=True)
